e43790474dfd857d502fb241d0e162f59fca7d5b
Joshua Flores <joshuaflores@joshuas-mbp.myfiosgateway.com>
2021-05-28 23:11:30 -0400

    completed all core functionality

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const gameBoard = (() => {
       squares[squareId].innerHTML = turn.value;
     });
   };
-  return { renderBoard };
+
+  //display who's turn it is
+  const displayPlayer = (player) => {
+    document.getElementById("header").innerHTML = `
+    <div class="gameboard-header">
+      <span>${player}'s turn</span>
+    </div>`;
+  };
+
+  return { renderBoard, displayPlayer };
 })();
 
 const playerFactory = (name, mark) => {
@@ -21,6 +30,7 @@ let Player2 = "";
 
 // gameboard controller
 const gameController = (() => {
+  //action when user clicks a form
   const form = document.getElementById("startForm");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -34,6 +44,7 @@ const gameController = (() => {
     Player2 = playerFactory(player2Name, "O");
     form.setAttribute("style", "display:none");
     document.getElementById("board").setAttribute("style", "display:grid");
+    gameBoard.displayPlayer(Player1.getName());
   });
 
   //initialize the game board with values
@@ -49,6 +60,8 @@ const gameController = (() => {
     { square: 8, value: "" },
   ];
 
+  let gameIsActive = true;
+
   //initialize the current player
   let player1Turn = true;
 
@@ -56,16 +69,23 @@ const gameController = (() => {
   for (let i = 0; i < 9; i++) {
     document.getElementById(`square${i}`).addEventListener("click", () => {
       // do something when user clicks
-      addTurn(i, player1Turn ? Player1.getMark() : Player2.getMark());
 
-      // swap whos turn it is
-      player1Turn = !player1Turn;
+      // first check if the game is active...
+      if (gameIsActive) {
+        addTurn(i, player1Turn ? Player1.getMark() : Player2.getMark());
 
-      //render the board
-      gameBoard.renderBoard(turnHistory);
+        // swap whos turn it is
+        player1Turn = !player1Turn;
 
-      //check for a winner
-      checkForWinner();
+        //render the board
+        gameBoard.renderBoard(turnHistory);
+        gameBoard.displayPlayer(
+          player1Turn ? Player1.getName() : Player2.getName()
+        );
+
+        //check for a winner
+        checkForWinner();
+      }
     });
   }
 
@@ -90,10 +110,14 @@ const gameController = (() => {
       .map((turn) => turn.square);
 
     const numTurns = turnHistory.filter((turn) => turn.value);
-    console.log(numTurns.length);
     const checkMoves = (player, moves) => {
-      const alertWinner = () =>
-        setTimeout(() => alert(`${player} has won the game!`), 200);
+      const alertWinner = () => {
+        document.getElementById("header").innerHTML = `
+        <div class="gameboard-header">
+          <span>🎉&nbsp; ${player} wins! &nbsp;🎉</span>
+        </div>`;
+      };
+
       if (
         (moves.includes(0) && moves.includes(1) && moves.includes(2)) ||
         (moves.includes(3) && moves.includes(4) && moves.includes(5)) ||
@@ -101,16 +125,36 @@ const gameController = (() => {
         (moves.includes(0) && moves.includes(4) && moves.includes(8)) ||
         (moves.includes(2) && moves.includes(4) && moves.includes(6))
       ) {
+        gameIsActive = false;
         alertWinner();
       } else if (numTurns.length === 9) {
-        setTimeout(() =>
-          alert("Game ends in tie! Better luck next time guys.", 200)
-        );
+        gameIsActive = false;
+        document.getElementById("header").innerHTML = `
+        <div class="gameboard-header">
+          <span>TIE! womp womp...</span>
+        </div>`;
       }
     };
     checkMoves(Player1.getName(), player1Moves);
     checkMoves(Player2.getName(), player2Moves);
   };
+
+  //reset the game
+  const resetButton = document.getElementById("btn-reset");
+  resetButton.addEventListener("click", () => {
+    turnHistory.splice(0, turnHistory.length);
+    for (let i = 0; i < 9; i++) {
+      turnHistory.push({ square: i, value: "" });
+    }
+    gameIsActive = true;
+    player1Turn = true;
+    gameBoard.renderBoard(turnHistory);
+    console.log(turnHistory);
+    gameBoard.displayPlayer(
+      player1Turn ? Player1.getName() : Player2.getName()
+    );
+    console.log(gameIsActive);
+  });
 
   return { turnHistory };
 })();
